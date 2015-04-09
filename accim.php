a11b3d639ddcdee504fcd81ef4ef021d1bb3af93
Lee Guo Cheng <programmer17@gmail.com>
2015-04-09 18:39:06 +0800

    accim invention report...

---
+++ accim.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+
+<title>Project Report</title>
+
+<style>
+#container{
+	position:relative;
+}
+#container *{
+	position:relative;
+	word-warp:break;
+}
+label{
+	font-weight:bold;
+	font-size:20px;
+}
+.p1{
+	display:inline-block;
+	width:200px;
+}
+.p2{
+	display:block;
+	width:auto
+}
+.t1{
+	font-size:18px;
+	display:inline-block;
+	width:200px;
+	position:absolute;
+	left:20px;
+}
+.t2{
+	display:block;
+	width:200px;
+	font-size:20px;
+	position:absolute;
+	left:20px;
+}
+.d1{
+	position:absolute;
+	left:40px;
+	display:block;
+	width:calc(100% - 40px);
+}
+</style>
+
+</head>
+
+<body>
+<div id="container" style="max-width:960px;border:3px solid #ccc;padding:30px;margin:auto;color:#666">
+
+<label class="p1">Project:</label>	Student Platform
+<br><br>
+
+<label class="p1">The Condition Now:</label>	less of functions
+<br><br>
+
+<label class="p2">The functions which is available now:</label>
+<a class="t1">-Storytelling</a>	--	share something to whole school students
+<br>
+<a class="t1">-Ask & Answer</a>	--	ask and answer
+<br>
+<a class="t1">-Notice board</a>	--	admin post highlight notice
+<br>
+<a class="t1">-Report</a>	--	report unhealthy post
+<br>
+----------Extra----------
+<br>
+Some programless message can be contact with programmer student to suggest to post in special highlight wat to show to other user. 	E.g : ads
+
+<br><br>
+<label class="p2">Planed functions which is unavailable now:</label>
+
+<a class="t1">Class Blog</a>		--	The Senior 3 class can setup their own site (  E.g: J3x.chssibu.com )
+<br>
+<a class="t1">Club of school</a>	--	notice , discussions , site (E.g: science.chssibu.com )
+<br>
+<a class="t1">Student consultant</a>	--	Q&A ,  Anonymous letter
+<br>
+<a class="t1">Teamwork</a>		--	student can search another who is also have some need, hobby, mind or activities for easy to do with teamwork.
+
+<br><br>
+
+<label class="p2">Available functions details:</label>
+<a class="t2">Storytelling</a>
+<div class="d1">
+-	Students can post message through text , image , or hyperlink
+<br>
+-	Can post anonymously but can be identified by admin
+<br>
+-	Can do reply or like (+1)
+<br>
+-	Responsibility , user cannot delete their posted posts except admin
+</div>
+<br>
+<a class="t2">Ask & Answer</a>
+<div class="d1">
+-	Ask or answer through text , image or hyperlink
+<br>
+-	Asker can accept answers
+<br>
+-	Can do discussions but not answer
+<br>
+-	Can search questions
+</div>
+<br>
+<a class="t2">Report</a>
+<div class="d1">-	Report unhealthy content by it keywords, so admin can easily take action to another which have same keywords in posts</div>
+<br>
+<a class="t2">Notice</a>
+<div class="d1">-	Admin post highlight message</div>
+<br>
+<a class="t2">Notification</a>
+<div class="d1">-	Notification to personal user about the new status or event in ask, answer , post or else</div>
+<br>
+<a class="t2">Profile</a>
+<div class="d1">-	Students may change their profile's information customly</div>
+
+<br><br>
+<label class="p2">License:</label>
+<br>
+Horray! License is MIT License!<br>
+<small>(of course , how poor of its functions and code structer ?)</small><br>
+<br>
+<a style="width:auto" class="t2">The MIT License (MIT)</a>
+<br>
+<div class="d1">
+Copyright (c) 2015 cheng<br><br>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+<br><br>
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+<br><br>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+</div>
+<br>
+Github : https://github.com/cheng1999/Student-Platform
+
+<br><br>
+<label class="p2">Screenshots:</label>
+<br>
+<img src="screenshot.png" style="width:100%;"></img>
+<br>
+</div>
+</body>
+
+
+</html>
---
+++ l.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width">
+<title>呵</title>
+</head>
+<body>
+<div class="container" style="max-width:920px;margin:auto;padding:30px;padding-bottom:60px;height:auto;color:#777;border:3px solid #ccc">
+	
+		<span style="display:inline-block;width:30px"></span>今天领会的问题不是负责组的错，他本来就没有那个义务做这些琐碎的通知工作。<br><br>
+		是我从来就没有想参与事奉，对此也从来是不闻不问。<br><br>
+		我为自己对聚会程序造成的影响感到抱歉。
+		<br><br><br>
+		<b style="float:right;">李国成 2015/3/28</b>
+		
+</div>
+</body>
+</html>
